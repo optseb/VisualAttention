599b9dc4faebf40181238f542a8e023b5ecebafb
Seb James <seb.james@sheffield.ac.uk>
2019-03-13 16:52:54 +0000

    Ensures I can visualise on matlab or octave

--- analysis/vssurf.m
+++ analysis/vssurf.m
@@ -1,11 +1,26 @@
 % Plot a surface plot with some standard views.
-function vssurf (fign, data, field, time, surf_view=1, toplim=1, botlim=0)
+%function vssurf (fign, data, field, time, surf_view=1, toplim=1,
+%botlim=0)
+function vssurf (fign, data, field, time, surf_view, toplim, botlim)
+
+    % Note, default values for args is octave only. This should be portable
+    if nargin < 5
+        surf_view = 1;
+    end
+    if nargin < 6
+        toplim = 1
+    end
+    if nargin < 7
+        botlim = 0
+    end
+
     figure(fign)
     surf (data.(field)(:,:,time))
     % Escape underscores and set title:
     titlestr = regexprep (field, '_', '\_');
     title (titlestr);
-    zlim([botlim, toplim])
+    thelims = [botlim toplim]
+    zlim(thelims)
     % Add some useful views here:
     if (surf_view==1)
         view([1,85])
