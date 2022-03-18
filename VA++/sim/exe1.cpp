13ffd1c6d84e30b9ce99bf13db419148b32bb46b
Seb James <seb.james@sheffield.ac.uk>
2022-03-18 11:32:45 +0000

    Load gabor if possible to avoid recompute of projection

--- VA++/sim/exe1.cpp
+++ VA++/sim/exe1.cpp
@@ -10,12 +10,67 @@
 #include <morph/VisualDataModel.h>
 #include <morph/HexGridVisual.h>
 #include <morph/HexGrid.h>
+#include <morph/HdfData.h>
 
 #include "SpecialNet.h"
 #include "conn.h"
 
 #include <opencv2/opencv.hpp>
 #include <opencv2/imgproc.hpp>
+
+// A special HdfData class with a method to save/load std::vector<morph::nn::conn<float>> weight_table
+class WeightHdfData : public morph::HdfData
+{
+public:
+    WeightHdfData (const std::string fname,
+                   const morph::FileAccess _file_access = morph::FileAccess::TruncateWrite,
+                   const bool show_hdf_internal_errors = false)
+        : HdfData (fname, _file_access, show_hdf_internal_errors) {}
+
+    void read_weighttable (const char* path, std::vector<morph::nn::conn<float>>& vals)
+    {
+        // Read vectors, then resize/population the vector<conn<float>>.
+        std::string pth(path);
+        std::string i_path = pth + "_i";
+        std::string j_path = pth + "_j";
+        std::string w_path = pth + "_w";
+        std::vector<unsigned int> i_array (vals.size());
+        std::vector<unsigned int> j_array (vals.size());
+        std::vector<float> w_array (vals.size());
+        this->read_contained_vals (i_path.c_str(), i_array);
+        this->read_contained_vals (j_path.c_str(), j_array);
+        this->read_contained_vals (w_path.c_str(), w_array);
+        if (i_array.size() != j_array.size() || i_array.size() != w_array.size()) {
+            vals.resize(0);
+            return;
+        }
+        vals.resize (i_array.size());
+        for (unsigned int ii = 0; ii < i_array.size(); ++ii) {
+            vals[ii].i = i_array[ii];
+            vals[ii].j = j_array[ii];
+            vals[ii].w = w_array[ii];
+        }
+    }
+    void add_weighttable (const char* path, const std::vector<morph::nn::conn<float>>& vals)
+    {
+        // Each conn contains two unsigned ints and one float value. Make 3 vectors and save those.
+        std::vector<unsigned int> i_array (vals.size());
+        std::vector<unsigned int> j_array (vals.size());
+        std::vector<float> w_array (vals.size());
+        for (unsigned int ii = 0; ii < vals.size(); ++ii) {
+            i_array[ii] = vals[ii].i;
+            j_array[ii] = vals[ii].j;
+            w_array[ii] = vals[ii].w;
+        }
+        std::string pth(path);
+        std::string i_path = pth + "_i";
+        std::string j_path = pth + "_j";
+        std::string w_path = pth + "_w";
+        this->add_contained_vals (i_path.c_str(), i_array);
+        this->add_contained_vals (j_path.c_str(), j_array);
+        this->add_contained_vals (w_path.c_str(), w_array);
+    }
+};
 
 // This is a bit like a SpineCreator connectionFunc. Here, we connect from one HexGrid
 // to another with a Gaussian projection defined by the parameters in sigma. Result
@@ -59,30 +114,46 @@ std::vector<morph::nn::conn<float>> create_gabor (const morph::HexGrid& p1,
                                                   const float dir_s,
                                                   const float wco = 0.001f)
 {
+    // Create a filename
+    std::stringstream dss;
+    dss << "gabor_hg" << p1.num() << "_hg" << p2.num() << "_" << sigma_g << "_" << gain_g << "_" << lambda_s << "_" << gain_s << "_" << dir_s << "_" << wco << ".h5";
+
+    // Return object
     std::vector<morph::nn::conn<float>> weight_table;
-    // Connection is from p1 to p2. Loop through the output first.
-    for (auto h2 : p2.hexen) { // outputs
-        for (auto h1 : p1.hexen) { // inputs
-            // h2.x - h1.x and h
-            float d_x = h2.x - h1.x;
-            float d_y = h2.y - h1.y;
-            float dist = std::sqrt (d_x*d_x + d_y*d_y);
-            // Direction from source to dest
-            float dir_d = std::atan2 (d_y, d_x);
-            // Find the projection of the source->dest direction onto the sine wave direction. Call this distance dprime.
-            float dprime = dist * std::cos (dir_d + morph::mathconst<float>::two_pi
-                                            - ((dir_s * morph::mathconst<float>::two_pi)/360.0f));
-            // Use dprime to figure out what the sine weight is.
-            float sine_weight = gain_s * std::sin (dprime * morph::mathconst<float>::two_pi / lambda_s);
-            float gauss_weight = gain_g * std::exp(-0.5f * (dist/sigma_g) * (dist/sigma_g));
-            float combined_weight = sine_weight * gauss_weight;
-            if (std::abs(combined_weight) > wco) {
-                morph::nn::conn<float> c = {h1.vi, h2.vi, combined_weight};
-                weight_table.push_back (c);
+
+    if (morph::Tools::fileExists (dss.str())) {
+        WeightHdfData d (dss.str(), morph::FileAccess::ReadOnly);
+        std::cout << "Load gabor " << dss.str() << std::endl;
+        d.read_weighttable ("/wt", weight_table);
+    } else {
+        // Connection is from p1 to p2. Loop through the output first.
+        for (auto h2 : p2.hexen) { // outputs
+            for (auto h1 : p1.hexen) { // inputs
+                // h2.x - h1.x and h
+                float d_x = h2.x - h1.x;
+                float d_y = h2.y - h1.y;
+                float dist = std::sqrt (d_x*d_x + d_y*d_y);
+                // Direction from source to dest
+                float dir_d = std::atan2 (d_y, d_x);
+                // Find the projection of the source->dest direction onto the sine wave direction. Call this distance dprime.
+                float dprime = dist * std::cos (dir_d + morph::mathconst<float>::two_pi
+                                                - ((dir_s * morph::mathconst<float>::two_pi)/360.0f));
+                // Use dprime to figure out what the sine weight is.
+                float sine_weight = gain_s * std::sin (dprime * morph::mathconst<float>::two_pi / lambda_s);
+                float gauss_weight = gain_g * std::exp(-0.5f * (dist/sigma_g) * (dist/sigma_g));
+                float combined_weight = sine_weight * gauss_weight;
+                if (std::abs(combined_weight) > wco) {
+                    morph::nn::conn<float> c = {h1.vi, h2.vi, combined_weight};
+                    weight_table.push_back (c);
+                }
             }
         }
+        WeightHdfData d (dss.str(), morph::FileAccess::TruncateWrite);
+        std::cout << "Save gabor " << dss.str() << std::endl;
+        d.add_weighttable ("/wt", weight_table);
     }
-    std::cout << "Gabor weight table size: " << weight_table.size() << " (" << (weight_table.size() / p1.num()) << " connections per neuron)\n";
+    std::cout << "Gabor weight table size: " << weight_table.size()
+              << " (" << (weight_table.size() / p1.num()) << " connections per neuron)\n";
     return weight_table;
 }
 
@@ -125,7 +196,7 @@ int main()
                                                                       gain_s,
                                                                       dir_s_1);
     auto c = snet.connections.begin();
-    //c->setweight (weight_table1);
+    c->setweight (weight_table1);
 
     std::vector<morph::nn::conn<float>> weight_table2 = create_gabor (hg0, hg0,
                                                                       hg0.getd()/2.0f,
@@ -134,7 +205,7 @@ int main()
                                                                       gain_s,
                                                                       dir_s_2);
     c++;
-    //c->setweight (weight_table2);
+    c->setweight (weight_table2);
 
     // Load an image
     //std::string fn = "../sim/Lbig.png";
